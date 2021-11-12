d83ad1de1db5b2de5f2eec3bba9fcfb2f8c213e5
Mateo Ginocchio <ginocchiomateo@gmail.com>
2021-11-11 22:42:44 -0300

    Modificando Header

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="headerr">
+            <h1>Ojotas Impresas</h1>
+        </div>
+    )
+}
+
+export default Navbar
